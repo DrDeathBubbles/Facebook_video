b28546ba5bb45f995013364a8d76c38e8e713086
DrDeathBubbles <aaron.meagher@gmail.com>
2017-09-13 12:25:44 +0100

    First initialising of the video upload tool

--- video_upload.py
+++ video_upload.py
@@ -1,3 +1,8 @@
 # A python programme for the uploading of videos to facebook
 import requests
 import os
+
+url = 'https://graph-video.facebook.com/LSWSTST/videos?access_token={}'.format(os.environ['ACCESSTOKEN'])
+video_path ='/Users/aaronmeagher/Desktop/test_trimmed.mp4'
+files = {'file':open(video_path,'rb')}
+flag = requests.post(url,files=files)
