705d880f36301bf896c6029ed3e21379f8de577b
DrDeathBubbles <aaron.meagher@gmail.com>
2017-10-18 15:34:13 +0100

    Getting the video url for a facebook video

--- video_upload_2.py
+++ video_upload_2.py
@@ -2,12 +2,8 @@ import requests
 import os 
 access_token = os.environ['ACCESSTOKEN_VIDEO_2']
 
-
-
-
-
 def upload_video(video_path):
-    url = 'https://graph-video.facebook.com/LSWSTST/videos?access_token={}'.format(access_token_2) 
+    url = 'https://graph-video.facebook.com/LSWSTST/videos?access_token={}'.format(access_token) 
     _file = {'file':open(video_path,'rb')}
     flag = requests.post(url,files=_file) 
     return flag
@@ -18,4 +14,9 @@ def adding_description(post_id,description):
     url = 'https://graph.facebook.com/v2.10/{}?access_token={}'.format(post_id,access_token)
     flag = requests.post(url,json=data)
     return flag
-    #1424450227676325
+    #4244502276763251
+
+def reading_video_url(post_id):
+    url = 'https://graph.facebook.com/v2.10/{}?fields=permalink_url&access_token={}'.format(post_id,access_token)
+    flag = requests.post(url)
+    return flag
