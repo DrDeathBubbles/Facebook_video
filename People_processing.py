a038a3d50b9836232a60c7f424011e0efc47654d
DrDeathBubbles <aaron.meagher@gmail.com>
2017-10-23 19:58:14 +0100

    Adding what has to be done with it

--- People_processing.py
+++ People_processing.py
@@ -24,25 +24,31 @@ def get_spreadsheet(spreadsheet):
 def fuzzy_matching(talk_title, file_title):
      return fuzz.ratio(talk_title, file_title)
 
-def get_speakers(talk_title, field_title,data):
+def get_speakers(field_title,data):
     location = data['Title'].apply(fuzzy_matching,file_title=field_title)
-    location = location.idmax()
+    location = location.idxmax()
     speakers = []
     for i in range(1,5):
-        speaker = data.ix[location]['Speaker' + str(i)]
+        speaker = data.ix[location]['Speaker ' + str(i)]
         if len(speaker) > 0:
             speaker = speaker.split(',')
-            speakers.append(speaker)
+            speakers.append(speaker[0])
 
     return speakers
 
 if __name__ == '__main__':
     data = get_spreadsheet('WS_16_Speakers')
-    location = data['Title'].apply(fuzzy_matching,file_title='How to prevent a cyberwar')
-    location = location.idxmax()
-    speakers = []
-    for i in range(1,5):
-        speaker = data.ix[location]['Speaker' + str(i)]
-        if len(speaker) > 0:
-            speaker = speaker.split(',')
-            speakers.append(speaker)
+    speakers = get_speakers('Marketing in a mobile first world',data)
+
+    """
+    Need now to lookup the email addresses of these people 
+    """
+#    location = data['Title'].apply(fuzzy_matching,file_title='How to prevent a cyberwar')
+#    location = location.idxmax()
+#    speakers = []
+#    for i in range(1,5):
+#        speaker = data.ix[location]['Speaker' + str(i)]
+#        if len(speaker) > 0:
+#            speaker = speaker.split(',')
+#            speakers.append(speaker)
+#
