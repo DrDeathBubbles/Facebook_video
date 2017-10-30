82777e7cbb96e89bfa9b2d3b53f46f09b2cdeb51
DrDeathBubbles <aaron.meagher@gmail.com>
2017-10-30 10:59:11 +0000

    Removing print statements from video_processing.py

--- video_processing.py
+++ video_processing.py
@@ -11,10 +11,10 @@ def video_processing(video_file,start_time, end_time,output):
     clip.write_videofile(output)
 
 if __name__ == '__main__':
-    print(sys.argv[1])
-    print(sys.argv[2])
-    print(sys.argv[3])
-    print(sys.argv[4])
+    #print(sys.argv[1])
+    #print(sys.argv[2])
+    #print(sys.argv[3])
+    #print(sys.argv[4])
     video_processing(sys.argv[1], sys.argv[2],sys.argv[3], sys.argv[4])
 #    video_trimming('/Users/aaronmeagher/Desktop/test_trimmed.mp4')
 #    video_processing('/Users/aaronmeagher/Desktop/Trimmed_2.mp4',(10,15),(11,15),'~/Desktop/test_trimmed.mp4')
