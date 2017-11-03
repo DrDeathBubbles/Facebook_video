865a3775afc3e01181d918dfa3e71030d9965623
DrDeathBubbles <aaron.meagher@gmail.com>
2017-11-03 23:49:42 +0000

    Adding slugify

--- Making_test_video_files.py
+++ Making_test_video_files.py
@@ -1,8 +1,23 @@
 import pandas as pd 
 import os
+import unicodedata
+
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    """
+    
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    value = unicode(re.sub('[-\s]+', '-', value))
+
+
 
 
 def making_files(filename):
+    print(filename)
+    filename = slugify(filename)
     print(filename)
     os.system('cp test.mp4 /home/ubuntu/AJM/video_files/test/{}.mp4'.format(filename))
 
