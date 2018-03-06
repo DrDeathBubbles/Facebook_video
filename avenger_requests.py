d2adbdeb8ccd92688b89e0da0bedda22a11f7655
DrDeathBubbles <aaron.meagher@gmail.com>
2018-03-06 14:59:11 +0000

    Adding general and specific Putting avenger token in bash_profile

--- avenger_requests.py
+++ avenger_requests.py
@@ -1,10 +1,16 @@
 #This is the section which pulls information from avenger
-
+#Need to put in catch if the talk or the stage is not found!
 import requests
 import json 
+import os 
 
-def avenger_get_talk():
+
+def avenger_get_talks():
     out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/timeslots/')
+    return out
+
+def avenger_get_talk(id):
+    out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/timeslots/' + str(id))
     return out
 
 
@@ -12,7 +18,12 @@ def avenger_get_locations():
     out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/timeslot_locations')
     return out
 
+def avenger_get_locations(timeslot_location_id):
+    out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/timeslot_locations/' + str(timeslot_location_id))
+    return out
+
+
 def avenger_get_attendee_data():
-    headers = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3MDU5ZGUzNC01NjZmLTQyNWMtYWQ5YS01ZTAyN2VkZDhjYWEiLCJqdGkiOiIwN2QzYjgyMy1iN2Y3LTQ2NjctYTRmMi04OTcxZmMyNGM1MjIiLCJpYXQiOjE1MDkzODA0NDUsImFkbWluIjpmYWxzZSwiYXR0X2lkIjoiMTE5NjVjMzItMDJjYy00Y2Y1LWExOTYtY2IyYzIxZDk3YzY5In0.jdMksOC8UI2zj5IgIw0NoT9iDsprT0UofYheD9vSxaE'}
+    headers = {'Authorization': 'Bearer {}'.format(os.environ['avenger_token'])}
     out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/attendances',headers = headers)
     return out
