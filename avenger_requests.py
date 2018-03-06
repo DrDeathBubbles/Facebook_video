05d7f7b95374499f0f3bfac4871be0f6be30e3c0
DrDeathBubbles <aaron.meagher@gmail.com>
2018-03-06 14:43:26 +0000

    The three endpoints are now working basically

--- avenger_requests.py
+++ avenger_requests.py
@@ -3,6 +3,16 @@
 import requests
 import json 
 
-def get_talk():
-    a = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/timeslots/')
-    return a
+def avenger_get_talk():
+    out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/timeslots/')
+    return out
+
+
+def avenger_get_locations():
+    out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/timeslot_locations')
+    return out
+
+def avenger_get_attendee_data():
+    headers = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3MDU5ZGUzNC01NjZmLTQyNWMtYWQ5YS01ZTAyN2VkZDhjYWEiLCJqdGkiOiIwN2QzYjgyMy1iN2Y3LTQ2NjctYTRmMi04OTcxZmMyNGM1MjIiLCJpYXQiOjE1MDkzODA0NDUsImFkbWluIjpmYWxzZSwiYXR0X2lkIjoiMTE5NjVjMzItMDJjYy00Y2Y1LWExOTYtY2IyYzIxZDk3YzY5In0.jdMksOC8UI2zj5IgIw0NoT9iDsprT0UofYheD9vSxaE'}
+    out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/attendances',headers = headers)
+    return out
