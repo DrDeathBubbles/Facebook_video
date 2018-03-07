280cf030633473b9fecd6306bcefb03d41d87259
DrDeathBubbles <aaron.meagher@gmail.com>
2018-03-07 12:30:44 +0000

    We are now adding in self to class methods

--- avenger_requests.py
+++ avenger_requests.py
@@ -7,18 +7,18 @@ import os
 
 
 
-def class avenger_requests():
+class avenger_requests():
 
 
 
-    def get_talks():
+    def get_talks(self):
         """
         This gets the information for all the talks which are in the database
         """
         out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/timeslots/')
         return out
 
-    def get_talk(id):
+    def get_talks_particular(self, id):
         """
         This gets the information about a single talk identified by its UUID
         """
@@ -29,14 +29,14 @@ def class avenger_requests():
 
 
     #This gets the name of the stage based on the timeslot
-    def get_locations():
+    def get_locations(self):
         """
         This gets the information of all the stages and their names 
         """
         out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/timeslot_locations')
         return out
 
-    def get_location(timeslot_location_id):
+    def get_locations_particular(self, timeslot_location_id):
         """
         This gets the stage information, including name, based on the timeslot_location id
         """
@@ -47,7 +47,7 @@ def class avenger_requests():
 
     #This is the attendee information section 
 
-    def get_attendee_data():
+    def get_attendee_data(self):
         """
         This gets the attendee information for all attendees in Avenger
         """
@@ -56,10 +56,25 @@ def class avenger_requests():
         return out
 
 
-    def get_attendee_data(attendance_id):
+    def get_attendee_data_particular(self, attendance_id):
         """
         This gets the attendee information for a particular attendee in Avenger 
         """
         headers = {'Authorization': 'Bearer {}'.format(os.environ['avenger_token'])}
         out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/attendances/{}'.format(attendance_id),headers = headers)
         return out
+    
+    
+    def get_attendee_data_particular_2(self, attendance_id):
+        """
+        This gets the attendee information for a particular attendee in Avenger 
+        """
+        headers = {'Authorization': 'Bearer {}'.format(os.environ['avenger_token'])}
+        out = requests.get('https://avenger.cilabs.net/v1/conferences/ws17/attendances/{}'.format(attendance_id),headers = headers)
+        return out
+
+
+    def name_processing(self, id):
+        talk = self.get_talks_particular(id)
+        return talk
+
