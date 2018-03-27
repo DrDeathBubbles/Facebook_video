8bb06f573a5ae374f3c2588478528190dfd4f5a0
DrDeathBubbles <aaron.meagher@gmail.com>
2018-03-27 11:38:43 +0100

    Adding in method to produce long lasting token in the dataframe

--- rough_work.py
+++ rough_work.py
@@ -1,3 +1,23 @@
+import pandas as pd 
+from FB_access_token_generator import get_long_lasting_token_return
+
+
+
+def make_json(csv_file):
+    data = pd.read_csv(csv_file)
+    data.to_json('test.json')
+
+
+def generate_long_lasting_tokens_dataframe(data):
+    if 'long_lasting_token' not in data:
+        data['long_lasting_token'] = ''
+
+    token_generator = get_long_lasting_token_return    
+    data['long_lasting_token'] = data['access_token'].apply(token_generator)
+
+    return data 
+
+
 def search_key_2( stages, key):
     out = []
     for i in stages:
@@ -12,4 +32,4 @@ class test():
     a = 1
 
     def test_method(self):
-        print(a)
+        print(test.a)
