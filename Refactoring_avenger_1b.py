6545151cb4d734e544647459697dc9c68f4ca7c1
DrDeathBubbles <aaron.meagher@gmail.com>
2018-03-29 11:30:38 +0100

    Adding in print statements to refactoring

--- Refactoring_avenger_1b.py
+++ Refactoring_avenger_1b.py
@@ -222,7 +222,7 @@ def processing_message(process_name,tasks,results):
                 talk_location_id = avenger.get_timeslot_id(uuid)
                 fb_page_id = int(data[data['id']==talk_location_id]['page_id'])
                 access_token = data[data['id']==talk_location_id]['long_lasting_token'].values[0]
-                print('Got credentials')
+                print('Got')
             except Exception as e:
                 logging.error('Failed to get the credentials')
                 print('Failed to get credentials')
@@ -236,6 +236,7 @@ def processing_message(process_name,tasks,results):
 
                 print('Post to facebook unsuccessful')
                 continue 
+            
             #try:
             #    post = upload_video(file_location + 'edited_videos/' + message, fb_page_id, access_token)
             #    #if post.status_code == '400':
