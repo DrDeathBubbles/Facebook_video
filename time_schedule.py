52146cfc5714bc39f3d4c6380a1fbc65bd25132c
DrDeathBubbles <aaron.meagher@gmail.com>
2018-04-03 11:06:09 +0100

    Changing of time_schedule

--- time_schedule.py
+++ time_schedule.py
@@ -2,6 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import avenger_requests
+import time 
 
 
 def get_spreadsheet(spreadsheet):
@@ -21,25 +22,55 @@ def get_spreadsheet(spreadsheet):
     return sheet
 
 
-def get_stage_name(timeslot_location_id,locations):
-    temp = locations[locations['id']==timeslot_location_id]['name']
-    return temp.values[0]
 
 
 if __name__ == '__main__':
     sheet = get_spreadsheet('CC_18_timesheet')
     avenger = avenger_requests.avenger_requests()
-    talks = avenger.get_talks()
-    talks = pd.DataFrame(talks.json()['data'])
-    talks = talks[['title','description','start_time','end_time','timeslot_location_id','id']]
+    while True:
+        talks = avenger.get_talks()
+        talks = pd.DataFrame(talks.json()['data'])
+        talks = talks[['title','description','start_time','end_time','timeslot_location_id','id']]
 
-    locations = avenger.get_locations()
-    locations = pd.DataFrame(locations.json()['data'])
-    find_location = lambda x: locations[locations['id']== x ]['name'].values[0] 
+        locations = avenger.get_locations()
+        locations = pd.DataFrame(locations.json()['data'])
+        find_location = lambda x: locations[locations['id']== x ]['name'].values[0] 
 
-    talks['timeslot_location_id'] = talks['timeslot_location_id'].apply(find_location)
+        talks['timeslot_location_id'] = talks['timeslot_location_id'].apply(find_location)
 
-    temp = []
-    for row in talks.iterrows():
-        index,data = row
-        temp.append(data.tolist())
+        temp = []
+        for row in talks.iterrows():
+            index,data = row
+            temp.append(data.tolist())
+
+        for i in range(0,len(temp)):
+            print(i)
+            sheet.insert_row(temp[i],i+2)
+
+
+
+#    sheet = get_spreadsheet('CC_18_timesheet')
+#    avenger = avenger_requests.avenger_requests()
+#    columns = ['title','description','start_time','end_time','timeslot_location_id','id'] 
+#    index = pd.RangeIndex(0,220)
+#    old_talks = pd.DataFrame(index = index, columns = columns)
+
+
+#    while True:
+#
+#        new_talks = avenger.get_talks()
+#        new_talks = pd.DataFrame(new_talks.json()['data'])
+#        new_talks = talks[['title','description','start_time','end_time','timeslot_location_id','id']]
+#        ne_stacked = (old_talks != new_talks).stack() 
+#
+#
+#
+#
+#
+#        time.sleep(60*60)
+#
+
+
+
+
+
