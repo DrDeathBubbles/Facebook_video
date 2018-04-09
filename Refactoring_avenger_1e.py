912102faf5e4e30efbcbf319d214227e75d50138
DrDeathBubbles <aaron.meagher@gmail.com>
2018-04-09 12:35:34 +0100

    Putting back in original try except for posting to facebook

--- Refactoring_avenger_1e.py
+++ Refactoring_avenger_1e.py
@@ -272,26 +272,26 @@ def processing_message(process_name,tasks,results,fb_cred_data):
                 continue 
 
 
-            try:
-                post = upload_video(file_location + 'edited_videos/' + message, fb_page_id, access_token)
-                print('Posted to facebook')
-            except:
-
-                print('Post to facebook unsuccessful')
-                continue 
-            
             #try:
             #    post = upload_video(file_location + 'edited_videos/' + message, fb_page_id, access_token)
-            #    #if post.status_code == '400':
-            #    #    print('Uploads are blocked')
-            #    #    time.sleep(60*60*6)
-            #    #    continue
+            #    print('Posted to facebook')
+            #except:
+
+            #    print('Post to facebook unsuccessful')
+            #    continue 
+            
+            try:
+                post = upload_video(file_location + 'edited_videos/' + message, fb_page_id, access_token)
+                if post.status_code == '400':
+                    print('Uploads are blocked')
+                    time.sleep(60*60*6)
+                    continue
            
-            ##except:
-            ##    logging.error('Failed to post to facebook')
-            ##    print('Failed to post to facebook')
-            ##    logging.error(e)
-            ##    continue
+            except:
+                logging.error('Failed to post to facebook')
+                print('Failed to post to facebook')
+                logging.error(e)
+                continue
 
             #except:
             #    print('Failed to post to Facebook 2')
