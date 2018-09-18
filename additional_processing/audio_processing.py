117ae1be93027321e8712c37e72034816efb6573
DrDeathBubbles <aaron.meagher@gmail.com>
2018-09-18 12:01:08 +0100

    Moving audio processing to additional processing folder

--- additional_processing/audio_processing.py
+++ additional_processing/audio_processing.py
@@ -11,8 +11,6 @@ if __name__ == '__main__':
 
     file_location = '/home/ubuntu/AJM/video_files/'
 
-    skip_list = []
-
 
     s3 = boto3.resource('s3')
 
@@ -20,7 +18,7 @@ if __name__ == '__main__':
 
     get_last_modified = lambda obj: int(obj.last_modified.strftime('%s'))
 
-    unsorted = ['CS_8_10_Opening Remarks.mp4']
+    unsorted = []
 
     for file in my_bucket.objects.filter():
         unsorted.append(file)
@@ -32,28 +30,19 @@ if __name__ == '__main__':
     total = len(files) 
 
     for file in files:
-
-        if file in skip_list:
-            continue
-        else:
-            if 'mp4' in file:
-                i = i + 1
-                my_bucket.download_file(file, file_location + file )
-                try:
-                    temp = AudioFileClip.AudioFileClip(file_location + file)
-                except:
-                    print('Oops, cannot process this audio file')
-                    with open('Failures.txt','a') as f:
-                        f.write(file)
-                    continue     
-                audio_file =  file.rstrip('.mp4') + '.mp3' 
-                temp.write_audiofile(file_location + 'audio/' + audio_file) 
-                my_bucket.upload_file(file_location + 'audio/' + audio_file, 'WS17_audio/{}'.format(audio_file))
-                os.remove(file_location + file)
-
-            else:
-                continue
-
+        i = i + 1
+        my_bucket.download_file(file, file_location + file )
+        try:
+            temp = AudioFileClip.AudioFileClip(file_location + file)
+        except:
+            print('Oops, cannot process this audio file')
+            with open('Failures.txt','a') as f:
+                f.write(file)
+            continue     
+        audio_file =  file.rstrip('.mp4') + '.mp3' 
+        temp.write_audiofile(file_location + 'audio/' + audio_file) 
+        my_bucket.upload_file(file_location + 'audio/' + audio_file, 'WS17_audio/{}'.format(audio_file))
+        os.remove(file_location + file)
         print(i/total)
         print(file)
 
