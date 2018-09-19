a7461f814c3f53f14b815748d2abe1278a933650
DrDeathBubbles <aaron.meagher@gmail.com>
2018-09-19 14:34:56 +0100

    Adding space

--- main.py
+++ main.py
@@ -32,6 +32,9 @@ exclusion_list = ['f2cdfee8-0ccc-46b3-945c-c7759ee755ea']
 
 #path_to_videos = "/Users/aaronmeagher/AJM/video_files/"'
 path_to_videos = "/home/ubuntu/AJM/video_files/"
+
+
+
 s3 = boto3.resource('s3')
 
 
