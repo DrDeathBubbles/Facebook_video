d09a8607ab79504e4fa863d3050861f21f4cc0f5
DrDeathBubbles <aaron.meagher@gmail.com>
2018-10-01 09:50:57 +0100

    Building up client

--- youtube/video_categories.py
+++ youtube/video_categories.py
@@ -1,5 +1,17 @@
 # Sample python code for videoCategories.list
 
+CLIENT_SECRETS_FILE = "../credentials/client_secret.json"
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+def get_authenticated_service():
+  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+  credentials = flow.run_console()
+  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+
+
 def video_categories_list(client, **kwargs):
   # See full sample for function
   kwargs = remove_empty_kwargs(**kwargs)
