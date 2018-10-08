9f753ebca0b821961e5e3369c63cbd7406c2c8e8
DrDeathBubbles <aaron.meagher@gmail.com>
2018-10-08 14:43:39 +0100

    Removing extras from read_singe_range

--- Schedule_rough_work.py
+++ Schedule_rough_work.py
@@ -1,8 +1,10 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.discovery import build
+from httplib2 import Http
 import pandas as pd
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
+#from fuzzywuzzy import fuzz
+#from fuzzywuzzy import process
 
 
 def get_spreadsheet(sheet_name):
@@ -17,4 +19,13 @@ def get_spreadsheet(sheet_name):
     sheet = sheet.get_all_records()
     sheet = pd.DataFrame(sheet)
     sheet.columns = sheet.iloc[0]
-    return sheet  
+    return sheet 
+
+
+
+def read_single_range(spreadsheet_id, range_name):
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('./access_tokens/client_secret.json', scope)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+    result = service.spreadsheets().values().get( spreadsheetId=spreadsheet_id, range=range_name).execute()
+    return result
