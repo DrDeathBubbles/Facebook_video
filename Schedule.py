19388ef31c18c9ef6c3fc889023a8fbc065495a7
DrDeathBubbles <aaron.meagher@gmail.com>
2018-10-09 12:26:15 +0100

    Moving old schedule code to old_code folder Renaming schedule_rough_work to schedule as it is now the main control for schedule from now on

--- Schedule.py
+++ Schedule.py
@@ -1,91 +1,89 @@
-from apiclient import errors
-from apiclient.http import MediaFileUpload
-from apiclient.discovery import build
-from oauth2client import file, client, tools
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.discovery import build
 from httplib2 import Http
+import pandas as pd
 import avenger_requests
-import time
-import pandas as pd  
-import arrow 
-# ...
+import arrow
+import string
+import time 
 
-def update_file(service, file_id, new_title, new_description, new_mime_type,
-                new_filename, new_revision):
-  """Update an existing file's metadata and content.
 
-  Args:
-    service: Drive API service instance.
-    file_id: ID of the file to update.
-    new_title: New title for the file.
-    new_description: New description for the file.
-    new_mime_type: New MIME type for the file.
-    new_filename: Filename of the new content to upload.
-    new_revision: Whether or not to create a new revision for this file.
-  Returns:
-    Updated file metadata if successful, None otherwise.
-  """
-  try:
-    # First retrieve the file from the API.
-    file = service.files().get(fileId=file_id).execute()
+def get_spreadsheet(sheet_name):
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('./access_tokens/client_secret.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open(sheet_name).sheet1
+    sheet = sheet.get_all_records()
+    sheet = pd.DataFrame(sheet)
+    sheet.columns = sheet.iloc[0]
+    return sheet 
 
-    # File's new metadata.
-    file['title'] = new_title
-    file['description'] = new_description
-    file['mimeType'] = new_mime_type
+def read_single_range(spreadsheet_id, range_name):
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('./access_tokens/client_secret.json', scope)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+    result = service.spreadsheets().values().get( spreadsheetId=spreadsheet_id, range=range_name).execute()
+    return result
 
-    # File's new content.
-    media_body = MediaFileUpload(
-        new_filename, mimetype=new_mime_type, resumable=True)
 
-    # Send the request to the API.
-    updated_file = service.files().update(
-        fileId=file_id,
-        body=file,
-        #newRevision=new_revision,
-        media_body=media_body).execute()
-    return updated_file
-  except Exception as e: # errors.HttpError, error:
-    print('An error occurred: %s' % e)
-    #print('An error occurred:')
-    return None
+def write_single_range(spreadsheet_id,range_name,values,value_input_option='RAW'):
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('./access_tokens/client_secret.json', scope)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+    body ={'values':values}
+    result = service.spreadsheets().values().update(
+    spreadsheetId=spreadsheet_id, range=range_name,
+    valueInputOption=value_input_option, body=body).execute()
 
 
 def convert_time_zone(time):
     time = arrow.get(time)
     time = time.shift(hours = +8)
-    return time.format('YYYY/MM/DD HH:mm:ss')
+    return time.format('YYYY/MM/DD HH:mm:ss')    
+
+
+def time_schedule_aquisition(slug):
+    avenger = avenger_requests.avenger_requests(slug)
+    talks = avenger.get_talks()
+    talks = pd.DataFrame(talks.json()['data'])
+    talks = talks[['title','description','start_time','end_time','timeslot_location_id','id']]
+    locations = avenger.get_locations()
+    locations = pd.DataFrame(locations.json()['data'])
+    find_location = lambda x: locations[locations['id']== x ]['name'].values[0] 
+
+    talks['timeslot_location_id'] = talks['timeslot_location_id'].apply(find_location)
+    talks['start_time'] = talks['start_time'].apply(convert_time_zone)
+    talks['end_time'] = talks['end_time'].apply(convert_time_zone)
+
+    return talks
+
+def write_dataframe_to_gsheets(sheet_id, df):
+    top_left_cell = 'A1'
+    num2alpha = dict(zip(range(1, 27), string.ascii_uppercase))
+    bottom_right_cell = num2alpha[len(df.columns)] + str(len(df) + 1)
+    cell_ranges = top_left_cell + ':' + bottom_right_cell
+
+    values = df.as_matrix().tolist()
+    values.insert(0,list(df.columns))
+
+    write_single_range(sheet_id, cell_ranges, values)
+
+
+def update_schedule(sheet_id, slug, update_frequency):
+    schedule_old = pd.DataFrame()
+    while True:
+        schedule_new = time_schedule_aquisition(slug)
+        if schedule_old.equals(schedule_new):
+            print('No updates to schedule')
+            pass
+        else:
+            write_dataframe_to_gsheets(sheet_id, schedule_new)
+            schedule_old = schedule_new
+            print('Schedule updated')
+
+        time.sleep(update_frequency)                        
 
 if __name__ == '__main__':
-    avenger = avenger_requests.avenger_requests()
-    talks_old = pd.DataFrame()
-    while True:
-        talks = avenger.get_talks()
-        talks = pd.DataFrame(talks.json()['data'])
-        talks = talks[['title','description','start_time','end_time','timeslot_location_id','id']]
-
-        locations = avenger.get_locations()
-        locations = pd.DataFrame(locations.json()['data'])
-        find_location = lambda x: locations[locations['id']== x ]['name'].values[0] 
-
-        talks['timeslot_location_id'] = talks['timeslot_location_id'].apply(find_location)
-        talks['start_time'] = talks['start_time'].apply(convert_time_zone)
-        talks['end_time'] = talks['end_time'].apply(convert_time_zone)
-
-        if talks_old.equals(talks):
-            print('No updates to talks')
-            pass
-        else:    
-
-            talks.to_csv('Monc_18_stages.csv')
-
-            #file_id = '1w_PRr6K2kQo-k0LssSMUoVJC9aDVrjCAtBFo5UueW7o'
-            #file_id = '13b7f2wqgLp2JP5QDbrPiBTgRzHD7wLED_L_m0SUo6QI'
-            file_id = '1LafAM4Ru3fZYEyt44J-Pixul0VV4Yfxmvu7hr5te-vg'
-            store = file.Storage('credentials.json')
-            creds = store.get()
-            drive_service = build('drive', 'v2', http=creds.authorize(Http()))    
-            a = update_file(drive_service, file_id, 'Monc_18_stages', 'Monc_18 stage informatiom', 
-            'application/octet-stream', './Monc_18_stages.csv','True')
-            print('Updated pushed')
-            talks_old = talks 
-        time.sleep(60*15)
+    a = read_single_range('1LafAM4Ru3fZYEyt44J-Pixul0VV4Yfxmvu7hr5te-vg','A1:D4')        
+    b = write_single_range('1LafAM4Ru3fZYEyt44J-Pixul0VV4Yfxmvu7hr5te-vg','A1:D4',[['This','Is','A','Test'],[1,2,3,4]])
