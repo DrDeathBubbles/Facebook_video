cea36abfde7a2da2663ef41759e8d3c589f7f79d
DrDeathBubbles <aaron.meagher@gmail.com>
2018-10-09 09:59:49 +0100

    Importing avenger_requests and removing un-needed comments

--- Schedule_rough_work.py
+++ Schedule_rough_work.py
@@ -3,6 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient.discovery import build
 from httplib2 import Http
 import pandas as pd
+import avenger_requests
 #from fuzzywuzzy import fuzz
 #from fuzzywuzzy import process
 
@@ -11,11 +12,7 @@ def get_spreadsheet(sheet_name):
     scope = ['https://spreadsheets.google.com/feeds']
     creds = ServiceAccountCredentials.from_json_keyfile_name('./access_tokens/client_secret.json', scope)
     client = gspread.authorize(creds)
-
-    # Find a workbook by name and open the first sheet
-    # Make sure you use the right name here.
     sheet = client.open(sheet_name).sheet1
-    # Extract and print all of the values
     sheet = sheet.get_all_records()
     sheet = pd.DataFrame(sheet)
     sheet.columns = sheet.iloc[0]
