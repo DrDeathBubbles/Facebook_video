7d28743506d2ad11624d9a66664d9a0fbec4cc4b
DrDeathBubbles <aaron.meagher@gmail.com>
2018-10-09 10:06:17 +0100

    Adding in convert_time_zone and time_schedule_aquisition This last function gets the talks and times from avenger and returns a dataframe with the correct information for scheduling

--- Schedule_rough_work.py
+++ Schedule_rough_work.py
@@ -18,8 +18,6 @@ def get_spreadsheet(sheet_name):
     sheet.columns = sheet.iloc[0]
     return sheet 
 
-
-
 def read_single_range(spreadsheet_id, range_name):
     scope = ['https://spreadsheets.google.com/feeds']
     creds = ServiceAccountCredentials.from_json_keyfile_name('./access_tokens/client_secret.json', scope)
@@ -36,6 +34,28 @@ def write_single_range(spreadsheet_id,range_name,values,value_input_option='RAW'
     result = service.spreadsheets().values().update(
     spreadsheetId=spreadsheet_id, range=range_name,
     valueInputOption=value_input_option, body=body).execute()
+
+
+def convert_time_zone(time):
+    time = arrow.get(time)
+    time = time.shift(hours = +8)
+    return time.format('YYYY/MM/DD HH:mm:ss')    
+
+
+def time_schedule_aquisition(slug):
+    avenger = avenger_requests.avenger_requests(slug)
+    talks = avenger.get_talks()
+    talks = pd.DataFrame(talks.json()['data'])
+    talks = talks[['title','description','start_time','end_time','timeslot_location_id','id']]
+    locations = avenger.get_locations()
+    locations = pd.DataFrame(locations.json()['data'])
+    find_location = lambda x: locations[locations['id']== x ]['name'].values[0] 
+
+    talks['timeslot_location_id'] = talks['timeslot_location_id'].apply(find_location)
+    talks['start_time'] = talks['start_time'].apply(convert_time_zone)
+    talks['end_time'] = talks['end_time'].apply(convert_time_zone)
+
+    return talks
 
 
 if __name__ == '__main__':
