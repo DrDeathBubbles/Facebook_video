6ff9406e63a05713e04befac4d488ba590a6f171
DrDeathBubbles <aaron.meagher@gmail.com>
2018-10-09 11:00:09 +0100

    Adding in sections to write a random dataframe to a gsheet

--- Schedule_rough_work.py
+++ Schedule_rough_work.py
@@ -5,6 +5,7 @@ from httplib2 import Http
 import pandas as pd
 import avenger_requests
 import arrow
+import string
 #from fuzzywuzzy import fuzz
 #from fuzzywuzzy import process
 
@@ -58,6 +59,28 @@ def time_schedule_aquisition(slug):
 
     return talks
 
+def write_dataframe_to_gsheets(sheet_id, df):
+    top_left_cell = 'A1'
+    
+    num2alpha = dict(zip(range(1, 27), string.ascii_uppercase))
+    bottom_right_cell = num2alpha[len(df.columns)] + str(len(df) + 1)
+
+    cell_range = top_left_cell + ':' + bottom_right_cell
+    write_single_range(sheet_id, cell_ranges, values)
+
+
+def update_schedule(sheet_id, slug, update_frequency):
+    schedule_old = pd.Dataframe()
+    while True:
+        schedule_new = time_schedule_aquisition(slug)
+        if schedule_old.equals(schedule_new)
+            print('No updates to schedule')
+            pass
+        else:
+            write_schedule_to_gsheets(sheet_id, schedule_new)
+            schedule_old = schedule_new
+
+    time.sleep(update_frequency)                        
 
 if __name__ == '__main__':
     a = read_single_range('1LafAM4Ru3fZYEyt44J-Pixul0VV4Yfxmvu7hr5te-vg','A1:D4')        
