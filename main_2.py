c9e8c194811e7573cbf44028afc109e42da74b62
DrDeathBubbles <aaron.meagher@gmail.com>
2018-10-28 13:44:28 +0000

    Changing s3 bucket location for audio

--- main_2.py
+++ main_2.py
@@ -138,7 +138,7 @@ def post_to_s3(file_location, message, output_file_name):
 def post_to_s3_audio(file_location, message, output_file_name):
     message = message.rstrip('.mp4') + '.mp3'
     my_bucket = s3.Bucket('ws18-videos')
-    a = my_bucket.upload_file(file_location +'edited_videos/audio/'+message, output_file_name)
+    a = my_bucket.upload_file(file_location +'edited_videos/audio/'+message, 'audio/' output_file_name)
     return a
 
 
