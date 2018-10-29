c5efcfe6b9f6db07fcc38b4f1d5c4335f57faa04
DrDeathBubbles <aaron.meagher@gmail.com>
2018-10-29 09:57:51 +0000

    Removing manula nltk analysis. Finished implementation of Rake

--- transcription_1.py
+++ transcription_1.py
@@ -12,119 +12,7 @@ from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from textblob import Word
 from time import sleep
-
-
-
-
-
-class text_analysis:
-
-    def __init__(self, input_text):
-        self.text = input_text
-        self.processed_text = input_text 
-
-    def number_of_words(self):
-        word_count = lambda x: len(str(x).split(" "))
-        n_words = word_count(self.text)            
-        return n_words
-
-    def number_of_characters(self):
-        return len(self.text) 
-
-    def average_word_length(self):
-        words = self.text.split(' ')
-        words = [len(i) for i in words]
-        awl = sum(words) / len(words)
-        return awl 
-
-    def number_of_stopwords(self):
-        stop = nltk.corpus.stopwords.words('english')
-        f = lambda x : len([x for x in x.split() if x in stop])
-        nsw = f(self.text)
-        return nsw
-
-    def number_of_special_characters(self):    
-        pass
-
-    def number_of_numerics(self):
-        pass
-
-
-    def make_lower_case(self):
-        f = lambda x: " ".join(x.lower() for x in x.split())
-        out = f(self.text)
-        self.processed_text = f(self.processed_text)
-        return out
-
-    def remove_punctuation(self):
-        table = str.maketrans({key: None for key in string.punctuation})
-        out = self.text.translate(table)
-        self.processed_text = self.processed_text.translate(table)
-        return out
-
-
-    def remove_stop_words(self):
-        stop = stopwords.words('english')
-        f = lambda x: " ".join(x for x in x.split() if x not in stop) 
-        out = f(self.text)
-        self.processed_text = f(self.processed_text)
-        return out 
-
-    def remove_common_words(self):
-        temp = self.text.split()
-        counter = collections.Counter(temp)
-        most_common = counter.most_common(10)
-        keys = [x[0] for x in most_common]
-        f = lambda x: " ".join(x for x in x.split() if x not in keys)
-        out = f(self.text)
-        self.processed_text = f(self.processed_text)
-        return out
-
-    def remove_rare_words(self):
-        temp = self.text.split()
-        counter = collections.Counter(temp)
-        least_common = counter.most_common()[-10:]
-        keys = [x[0] for x in least_common]
-        f = lambda x: " ".join(x for x in x.split() if x not in keys)
-        out = f(self.text)
-        self.processed_text = f(self.processed_text)
-        return out
-
-    def tokenise(self):
-        out = TextBlob(self.text).words
-        self.processed_text = TextBlob(self.processed_text).words
-        return out
-
-    def stemming(self):
-        st = PorterStemmer()
-        f = lambda x: " ".join([st.stem(word) for word in x.split()])
-        out = f(self.text)
-        self.processed_text = f(self.processed_text)
-        return out
-
-    def lemmatisation(self):
-        f = lambda x : " ".join([Word(word).lemmatize() for word in x.split()])
-        out = f(self.text)
-        self.processed_text = f(self.processed_text)
-        return out 
-
-    def pre_processing(self):
-        self.make_lower_case()
-        self.remove_punctuation()
-        self.remove_stop_words()
-        self.remove_common_words()
-        self.remove_rare_words()
-        #self.tokenise()
-        self.lemmatisation()
-
-
-    def n_gram(self, number):
-        return TextBlot(self.processed_text).n_gram(number)
-
-    def term_frequency(self):
-        pass
-
-
+from rake_nltk import Rake 
 
 
 def aws_transcribe(job_name,job_uri):
@@ -175,6 +63,12 @@ def save_text_to_s3(uuid, text):
     os.remove(file_name)
     return a
 
+def key_word_analysis(text):
+    r = Rake(min_length=2, max_length=4)
+    r.extract_keywords_from_text(text)
+    keywords = r.get_ranked_phrases()
+    return keywords 
+
 
 
 
@@ -183,11 +77,11 @@ def main():
     while True:
         messages = get_messages()
         for message in messages:
-            s3_url, uuid = out[0]['s3_url'], out[0]['uuid']
+            s3_url, uuid = message[0]['s3_url'], message[0]['uuid']
             response = aws_transcribe(uuid, s3_url)
             text = get_text(response)
             save_text_to_s3(uuid, text)
-
+            keywords = key_word_analysis(text)
 
 
 
