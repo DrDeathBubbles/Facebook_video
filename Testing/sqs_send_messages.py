4dc51fe6a0a0adc773a4632a04e48fd2e3d4df84
DrDeathBubbles <aaron.meagher@gmail.com>
2019-02-06 16:13:39 +0000

    SQS testing

---
+++ Testing/sqs_send_messages.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+message_body = """{"Records":[{"eventVersion":"2.1","eventSource":"aws:s3","awsRegion":"eu-west-1",
+"eventTime":"2019-02-05T15:01:25.803Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"AWS:AIDAJSJVDM427OYOMDUSA"},
+"requestParameters":{"sourceIPAddress":"89.101.27.89"},"responseElements":{"x-amz-request-id":"71BAF6E333676EA0",
+"x-amz-id-2":"fe/GVq3Othx5aHk7UkewTHeMFGO+dlGMcQvOQAOLUFUyh8y8WWRfj9ZqpHNVDtf+ULV0VgygQxU="},
+"s3":{"s3SchemaVersion":"1.0","configurationId":"DS_AJM_VIDEO_UPLOAD","bucket":{"name":"ds-ajm-videos",
+"ownerIdentity":{"principalId":"A3MRBPFDOQ44CE"},"arn":"arn:aws:s3:::ds-ajm-videos"},"object":{"key":"test.mp4","size":11340176,
+"eTag":"ca69605b2d5e5ade854af8381beff3d2","versionId":"Y1h8ir9D3i_fpDEgFAjPqFTHB6BRWTEj","sequencer":"005C59A544A94BC581"}}}]}"""
