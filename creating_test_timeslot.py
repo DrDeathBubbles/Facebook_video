f185a9d65e12e1e97878783c6f131301faabbc3a
DrDeathBubbles <aaron.meagher@gmail.com>
2019-05-08 11:27:30 +0100

    Changing video upload for local files

--- creating_test_timeslot.py
+++ creating_test_timeslot.py
@@ -6,7 +6,7 @@ r = redis.Redis(host='localhost', port = 6379, db=0)
 
 test_slot = {'title':'DS TEST TITLE', 'description':'DS TEST DESCRIPTION', 'start_time':'2019/05/21 13:45:00', 'end_time':'2019/05/21 14:00:00',
 'timeslot_location_id':'Centre Stage','id':'6a069e0c-ea96-4516-b047-ef919173test', 'speakers': 'Aaron Meagher', 'priority':'0', 'block': '0',
-'status': 'Unprocessed'}
+'status': 'Unprocessed','speakers_for_emails': 'Aaron Meagher'}
 
 
 r.hmset('6a069e0c-ea96-4516-b047-ef919173test Aaron Meagher', test_slot)
