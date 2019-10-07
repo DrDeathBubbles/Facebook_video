6d5b060ff3ba597ac2a862eb26c2b4fc5a0575db
Ubuntu <ubuntu@ip-10-0-0-128.eu-west-1.compute.internal>
2019-10-07 13:38:16 +0000

    Saftey commit

---
+++ access_tokens/People_processing_CC.py
@@ -0,0 +1,35 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+
+
+
+
+def fuzzy_matching(talk_title, file_title):
+    """
+    Fuzzy matching taking the talk title as the input
+    And the field_title as the title which is supplied
+    """
+    return fuzz.ratio(talk_title, file_title)
+
+
+
+def get_emails(field_title, data):
+    location = data['Full Name'].apply(fuzzy_matching,file_title=field_title)
+    location = location.idxmax()
+    emails = {}
+    emails[field_title] = []
+    for i in data.ix[location]['cc'].split(','):
+        emails[field_title].append(i.strip())
+    return emails 
+
+
+def get_emails_cc(field_title, data):
+    location = data['Full Name'].apply(fuzzy_matching,file_title=field_title)
+    location = location.idxmax()
+    primary_email = data.ix[location]['Email']
+    cc_email = data.ix[location]['cc']
+    return [primary_email, cc_email] 
