1935ac99c2aa0efd09c2f6f0bdfc041802e9ffcf
DrDeathBubbles <aaron.meagher@gmail.com>
2019-10-10 14:26:09 +0100

    Changing test timeslot to new schema

--- creating_test_timeslot.py
+++ creating_test_timeslot.py
@@ -6,7 +6,8 @@ r = redis.Redis(host='localhost', port = 6379, db=0)
 
 test_slot = {'title':'DS TEST TITLE', 'description':'DS TEST DESCRIPTION', 'start_time':'2019/05/21 13:45:00', 'end_time':'2019/05/21 14:00:00',
 'timeslot_location_id':'Centre Stage','id':'6a069e0c-ea96-4516-b047-ef919173test', 'speakers': 'Aaron Meagher', 'priority':'0', 'block': '0',
+'resend_emails':0,'set_private':0,'vimeo_link':'','youtube_link':'','s3_link_public':'','s3_link_raw':'',
 'status': 'Unprocessed','speakers_for_emails': 'Aaron Meagher'}
 
 
-r.hmset('Startup-University-999 Aaron Meagher', test_slot)
+r.hmset('6a069e0c-ea96-4516-b047-ef919173test Aaron Meagher', test_slot)
