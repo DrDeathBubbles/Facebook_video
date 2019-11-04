d05b6bf7ca6cc935c050b55d6b90f4d0f971ba7a
DrDeathBubbles <aaron.meagher@gmail.com>
2019-11-04 15:38:23 +0000

    Filling in gaps for schedule

---
+++ additional_processing/schedule_manual_generation.py
@@ -0,0 +1,75 @@
+import pandas as pd 
+import redis
+import uuid
+
+def speaker_name_processing(speakers):
+
+    if len(speakers) == 1:
+        speakers = speakers[0]
+
+    elif len(speakers) == 2:
+        speakers = ' and '.join(speakers)
+
+    elif len(speakers) > 2:
+        speakers = ', '.join(speakers[:-1]) + ' and ' + speakers[-1]
+
+    else:
+        speakers = ''    
+
+    return speakers  
+
+
+
+
+def row_processing(row):
+    output = {}
+    row = row[1]
+    id_ = uuid.uuid5(uuid.NAMESPACE_DNS, row['Talk'])
+
+    output['title'] = row['Talk']
+    output['Speaker'] = row['Speaker']
+    output['Stage'] = row['Stage']
+    output['priority'] = 0
+    output['block'] = 0
+    output['set_private'] = 0
+    output['resend_emails'] = 0
+    output['upload_to_youtube'] = 0
+    output['vimeo_link'] = ''
+    output['youtube_link'] = ''
+    output['s3_link_public'] = ''
+    output['s3_link_raw'] = ''
+    output['status'] = 'Unprocessed'
+    output['IGNORE_Emails_resent'] = 0
+    output['id'] = id_ 
+    output['uuid'] = id_
+    output['speakers_for_emails'] = row['Speakers']
+    output['speakers'] = speaker_name_processing(row['Speakers'].split(','))
+
+
+
+
+
+
+
+
+
+r = redis.Redis(host='localhost', port = 6379, db=0
+
+xls = pd.ExcelFile('Ws_19_stages.xlsx')
+
+stages_2 = pd.read_excel(xls, 'Stage 2')
+stages_3 = pd.read_excel(xls, 'Stage 3')
+stage_51 = pd.read_excel(xls, 'Stage 5, Day 1')
+stage_52 = pd.read_excel(xls, 'Stage 5, Day 2')
+stage_53 = pd.read_excel(xls, 'Stage 5, Day 3')
+stage_81 = pd.read_excel(xls, 'Stage 8, Day 1')
+stage_823 = pd.read_excel(xls, 'Stage 8, Day 2&3')
+
+
+
+
+for row in stages_2.iterrows:
+
+
+
+
