e1e98b2ecf159968bd171ce97bbdf223748bcc51
DrDeathBubbles <aaron.meagher@gmail.com>
2019-11-04 18:48:12 +0000

    Hacking email

--- People_processing_CC.py
+++ People_processing_CC.py
@@ -27,9 +27,21 @@ def get_emails(field_title, data):
     return emails 
 
 
-def get_emails_cc(field_title, data):
+def get_emails_cc_old(field_title, data):
     location = data['Full Name'].apply(fuzzy_matching,file_title=field_title)
     location = location.idxmax()
     primary_email = data.ix[location]['Email']
     cc_email = data.ix[location]['cc']
-    return [primary_email, cc_email] 
+    return [primary_email, cc_email]
+
+
+def get_emails_cc(field_title, data):
+    location = data['Full Name'].apply(fuzzy_matching,file_title=field_title)
+    location = location[location == 100]
+    if len(location) == 1:
+        primary_email = data.ix[location]['Email']
+        cc_email = data.ix[location]['cc']
+    else:
+        primary_email = ''
+        cc_email = ''    
+    return [primary_email, cc_email]      
