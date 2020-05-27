38244e54d63020ed936dccc541498a8ce0e7153c
DrDeathBubbles <aaron.meagher@gmail.com>
2020-05-27 13:41:58 +0100

    Updating

--- Schedule/schedule_redis_graphql.py
+++ Schedule/schedule_redis_graphql.py
@@ -62,7 +62,7 @@ def speaker_name_processing(speakers):
 
 def run_query(query):
     headers = {'x-event-id': 'cc20'}
-    request = requests.post('https://api.cilabs.com/graphql', json={'query': query})
+    request = requests.post('https://api.cilabs.com/graphql', headers = headers, json={'query': query})
     if request.status_code == 200:
         return request.json()
     else:
