b3589157867ca3de690b920c7c3976f2d3a4f21a
DrDeathBubbles <aaron.meagher@gmail.com>
2020-05-31 15:24:47 +0100

    Changing to participants_2

--- Schedule/schedule_redis_graphql.py
+++ Schedule/schedule_redis_graphql.py
@@ -91,7 +91,7 @@ def time_schedule_aquisition_2(talks):
     total = pd.concat(temp, ignore_index = True)
     total.fillna('', inplace = True)   
     total['location'] = total['location'].apply(get_locations)
-    total['speakers_seed'] = total['participants'].apply(get_participants)
+    total['speakers_seed'] = total['participants'].apply(get_participants_2)
     total['speakers'] = total['speakers_seed'].apply(speaker_name_processing)
     total['speakers_for_emails'] = total['speakers_seed'].apply(','.join)
 
