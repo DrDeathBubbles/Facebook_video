826fdd2f2f0ba18c00d05f5ccc1c0af9b4c1d94a
DrDeathBubbles <aaron.meagher@gmail.com>
2020-06-03 10:59:07 +0100

    Adding subtitle_upload

--- additional_processing/vimeo_library.py
+++ additional_processing/vimeo_library.py
@@ -37,6 +37,31 @@ def vimeo_upload(file_name, title, description,privacy='anybody'):
     return response['link']
 
 
+
+def subtitle_upload(video_uri,file_name,language, track_type = 'subtitles'):
+    client.subtitle_upload(video_uri,track_type,language,file_name)
+
+    
+
+
+    while True: 
+        response = client.get(uri + '?fields=transcode.status').json()
+        if response['transcode']['status'] == 'complete':
+          print('Your video finished transcoding.')
+          break
+        elif response['transcode']['status'] == 'in_progress':
+          print('Your video is still transcoding.')
+        else:
+          print('Your video encountered an error during transcoding.')
+          return -1 
+        time.sleep(60)  
+
+
+    response = client.get(uri + '?fields=link').json()
+    return response['link']
+
+
+
     if __name__ == "__main__":
       file_name = sys.argv[1]
       title = sys.argv[2]
