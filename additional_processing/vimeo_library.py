f26addc6e201a8e39fe6b1c6e7fa6eb8382f346b
DrDeathBubbles <aaron.meagher@gmail.com>
2020-06-04 15:23:54 +0100

    Adding subtitle track upload to vimeo upload

--- additional_processing/vimeo_library.py
+++ additional_processing/vimeo_library.py
@@ -35,33 +35,13 @@ def vimeo_upload(file_name, title, description,privacy='anybody'):
 
     response = client.get(uri + '?fields=link').json()
 
-    #return response['link']
-    return response
-
-
-def subtitle_upload(video_uri,file_name,language, track_type = 'subtitles'):
-    client.subtitle_upload(video_uri,track_type,language,file_name)
-    print('Force')
-
-    
-
-
-    while True: 
-        response = client.get(uri + '?fields=transcode.status').json()
-        if response['transcode']['status'] == 'complete':
-          print('Your video finished transcoding.')
-          break
-        elif response['transcode']['status'] == 'in_progress':
-          print('Your video is still transcoding.')
-        else:
-          print('Your video encountered an error during transcoding.')
-          return -1 
-        time.sleep(60)  
-
-
-    response = client.get(uri + '?fields=link').json()
     return response['link']
 
+
+def subtitle_upload(video_uri,file_name,language = 'en-US', track_type = 'subtitles'):
+    video_uri = '/videos/' + video_uri.split('/')[3]
+    out = client.subtitle_upload(video_uri,track_type,language,file_name)
+    return out
 
 
     if __name__ == "__main__":
