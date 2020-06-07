f879c7b54b19b47551efd5283db8bbd485c7091d
DrDeathBubbles <aaron.meagher@gmail.com>
2020-06-07 16:30:42 +0100

    Adding clean_up

--- CFH_main.py
+++ CFH_main.py
@@ -373,7 +373,7 @@ def processing_message(queue, configurer, process_name, tasks, input_bucket, out
 
             try:
                 file_locations = [file_location, './']
-                #clean_up(file_locations, key)
+                clean_up(file_locations, key)
                 print('removed local files')
 
 
