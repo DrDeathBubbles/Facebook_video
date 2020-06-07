e6ba8074d00c8109292d6afabb62d7f8908b8ef4
DrDeathBubbles <aaron.meagher@gmail.com>
2020-06-07 13:44:17 +0100

    Changing uuid processing

--- CFH_main.py
+++ CFH_main.py
@@ -527,7 +527,8 @@ def main(input_bucket, output_bucket,free_cores=5, priority_cores = 15 ):
 
             try:
                 message = message.lstrip(input_bucket + '/') 
-                uuid = message.split('_')[-3]
+                #uuid = message.split('_')[-3]
+                uuid = message.rstrip('.mp4')
                 keys = r.keys()
                 keys = [c for c in keys if uuid in c]
 
