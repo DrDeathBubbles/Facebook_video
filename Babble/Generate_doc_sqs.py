093002b14dff7cb68c4db3816084f2af79cae0f9
DrDeathBubbles <aaron.meagher@gmail.com>
2020-06-19 20:05:06 +0100

    Changing cred auth

--- Babble/Generate_doc_sqs.py
+++ Babble/Generate_doc_sqs.py
@@ -49,6 +49,37 @@ def service_generation():
 
     return [drive_service, service]
 
+def get_credentials():
+    """Gets valid user credentials from storage.
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'download-from-google-drive-to-s3.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, args)
+        else:  # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    
+    service = build('docs', 'v1', credentials=creds)
+    drive_service = build('drive', 'v3', credentials=creds)
+
+    
+    return [drive_service, service]    
+
 
 def doc_generation( drive_service,service,vimeo_url, s3_url, uid, speakers, text, title):
 
