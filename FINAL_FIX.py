7dbaecaa1b804ff421a4d2b1c13b0f3a19c4495e
DrDeathBubbles <aaron.meagher@gmail.com>
2020-07-20 20:21:10 +0100

    Finally work please potaato

--- FINAL_FIX.py
+++ FINAL_FIX.py
@@ -36,8 +36,8 @@ def drive_service():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES, access_type='offline')
-            creds = flow.run_local_server(port=0)
+                'credentials.json', SCOPES,)
+            creds = flow.run_local_server(port=8080)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
