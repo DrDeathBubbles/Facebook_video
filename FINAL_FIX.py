6afa842976fb14a866e35c2d27eb07652afd5b34
DrDeathBubbles <aaron.meagher@gmail.com>
2020-07-20 20:15:04 +0100

    offline

--- FINAL_FIX.py
+++ FINAL_FIX.py
@@ -36,7 +36,7 @@ def drive_service():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'credentials.json', SCOPES, access_type='offline')
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
