9e75d5782333d7f24ea07019e68e571ad5b166bc
DrDeathBubbles <aaron.meagher@gmail.com>
2020-07-22 14:59:31 +0100

    Uncommenting out

--- FINAL_FIX.py
+++ FINAL_FIX.py
@@ -98,10 +98,10 @@ done_keys = r.keys()
 links_to_be_processed  = links_to_be_processed[~links_to_be_processed['uuid'].isin(done_keys)]
 data = links_to_be_processed[['title','description','Finished Link','uuid']].values.tolist()
 
-for i in range(0,len(data)):
-    if data[i][3] in ['1S4FE1C9UITAB_6yiTgm_HV6KZCV2sBd2','13AmvwHAM2RP7xBJ-27ip4DYLAVatB96v','1kQww3LULTeQAh36UGDkO7DQuDy0z_PlJ','1fNTerQobUQX8mZrrzhsNeppnOYq9w8hP','1yPfDqKy8I1qGU8-bN9tg6Kp2BuoY_y6p','1JRXrJoC3LO5CHZsgDPX5LxmaTOghqiYw']:
-        data.pop(i)
-random.shuffle(data)
+#for i in range(0,len(data)):
+#    if data[i][3] in ['1S4FE1C9UITAB_6yiTgm_HV6KZCV2sBd2','13AmvwHAM2RP7xBJ-27ip4DYLAVatB96v','1kQww3LULTeQAh36UGDkO7DQuDy0z_PlJ','1fNTerQobUQX8mZrrzhsNeppnOYq9w8hP','1yPfDqKy8I1qGU8-bN9tg6Kp2BuoY_y6p','1JRXrJoC3LO5CHZsgDPX5LxmaTOghqiYw']:
+#        data.pop(i)
+#random.shuffle(data)
 
 saving_folder = '/home/ubuntu/AJM/video_files/'
 
