1b859bba19599a2b3b7c45d765a0813753817c88
DrDeathBubbles <aaron.meagher@gmail.com>
2020-11-24 22:09:59 +0000

    Random commit

--- WS20/data_processing.py
+++ WS20/data_processing.py
@@ -5,3 +5,10 @@ data =pd.read_excel('../Ws20_data/WS20_masterclass_data.xlsx', sheet_name = None
 
 tracks = data['TRACK']
 content = data['Content review Title & descript']
+final = data['Final schedule']
+joined = pd.merge(final, tracks, left_on = 'Title', right_on = 'Masterclass content title (do not edit)')
+
+out_cols = joined.columns
+blah = ['Masterclass content title (do not edit)', 'Title', 'Description',  'UUID folder']
+
+out = joined[blah]
